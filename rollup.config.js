461b93fbaedae46738913690159f033c09840908
Kevin Charles <char0042@umn.edu>
2018-04-03 09:01:59 -0500

    bundled up all converters with no dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,7 @@
 // rollup.config.js
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import builtins from 'rollup-plugin-node-builtins';
 
 export default {
   input: 'src/math-convert.js',
@@ -8,5 +11,10 @@ export default {
   },{
     file: 'build/math-convert_es6.js',
     format: 'es'
-  }]
+  }],
+  plugins: [
+    resolve(),
+    commonjs(),
+    builtins()
+  ]
 };
--- src/math-convert.js
+++ src/math-convert.js
@@ -1,3 +1,19 @@
-import  astToLatex  from './ast-to-latex';
+import astToGuppy from './ast-to-guppy';
+import astToLatex from './ast-to-latex';
+import astToMathjs from './ast-to-mathjs';
+import astToText from './ast-to-text';
 
+import latexToAst from './latex-to-ast';
+import mathjsToAst from './mathjs-to-ast';
+import textToAst from './text-to-ast';
+
+import mmlToLatex from './mml-to-latex';
+
+export {astToGuppy} ;
 export {astToLatex} ;
+export {astToMathjs} ;
+export {astToText} ;
+export {latexToAst} ;
+export {mathjsToAst} ;
+export {textToAst} ;
+export {mmlToLatex} ;
